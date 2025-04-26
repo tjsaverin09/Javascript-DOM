f77c76a05cc1bd46a0729299ecc8955c239e902f
Terence <tjsaverin98@live.com>
2025-04-26 09:03:48 -0500

    Javascript DOM2

--- app.js
+++ app.js
@@ -15,7 +15,30 @@
 //     document.querySelector('.title').style.color = 'red'
 // }
 
+// function toggleDarkMode() {
+//     document.querySelector('body').classList.toggle("dark-theme")
+// }
+
+//** 2nd Set Of Notes */
+
+
+// //First way of accessing an element
+// console.log(document.querySelector('.title'));
+
+// //Second way of accessing an element 
+// //which only works for an Id
+// console.log(document.getElementById('title'));
+
+// //Change HTML
+// document.querySelector('.title').innerHTML += ' Frontend Simplified'
+
+// // Change CSS
+// document.querySelector('.title').style.fontSize = "16px"
+
+// function chickenStrap() {
+//     document.querySelector('.title').style.color = 'red'
+// }
+
 function toggleDarkMode() {
     document.querySelector('body').classList.toggle("dark-theme")
 }
-
